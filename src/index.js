3a59ca19b2f817764b9c2f7b7eb6e8607aea57de
Harshit_Hacks <harshitparmar2162004@gmail.com>
2024-03-29 22:46:17 +0530

    changes executed

--- src/index.js
+++ src/index.js
@@ -2,10 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import LandingPage from './screens/landing_page/LandingPage';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <App />
+    <LandingPage/>
   </React.StrictMode>
 );
