e4943d37af38ebfbf2f2f2d0bcd13e5fa10bbbd1
Piyush Prajapati <piyushoa2004@gmail.com>
2024-03-29 23:26:47 +0530

    header updated

--- src/components/header/index.jsx
+++ src/components/header/index.jsx
@@ -1,4 +1,11 @@
 import style from "./style.module.scss";
 export const Header = () => {
-	return <></>;
+	return <>
+	<div className='w-full h-16 flex flex-row justify-between items-center px-10 border-b-gray-300 border-[1px]'>
+        <p className='text-primary-blue font-bold text-xl cursor-pointer'>Decentralized Voting System</p>
+        <div className='bg-primary-blue rounded-lg'>
+            <p className='text-white px-5 py-2 cursor-pointer'>{buttonText}</p>
+        </div>
+    </div>
+	</>;
 };
